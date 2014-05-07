c7a8304f6288c8a9bb014499e320d70c64d325c9
markdgzman <mark.dgzman@gmail.com>
2014-05-06 23:05:53 -0700

    Project Euler Problem 5
    Calculate the difference of the sum of squares and the square of sums of
    an input number.

---
+++ Sum Square Difference/Sum Square Difference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Square_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = 100;
+            SumSquare a = new SumSquare();
+
+            int sumSquare = a.sumSquare(inputNumber);
+            int squareSum = a.squareSum(inputNumber);
+
+            Console.WriteLine(sumSquare);
+            Console.WriteLine(squareSum);
+            //Difference
+            Console.WriteLine(Math.Abs(sumSquare - squareSum));
+            Console.Read();
+        }
+    }
+}
---
+++ Sum Square Difference/Sum Square Difference/SumSquare.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Square_Difference
+{
+    class SumSquare
+    {
+
+        public int sumSquare(int sumSquare)
+        {
+            int sum = 0;
+
+            for(int i =1; i < sumSquare+1; i++)
+            {
+                sum += i * i;
+            }
+            return sum;
+        }
+
+        public int squareSum(int squareSum)
+        {
+            int square = 0;
+            for(int i = 1; i < squareSum+1; i++)
+            {
+                square += i;
+            }
+            return square * square;
+        }
+    }
+}
